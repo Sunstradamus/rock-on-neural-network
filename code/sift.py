007a17ffb6e6d6a927dcd88a3701edebe9ee1d27
Andy <andy@example.com>
2016-11-25 00:23:17 -0800

    Added SIFT checker

---
+++ code/sift.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('Hand.jpg')
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+sift = cv2.SIFT()
+kp = sift.detect(img_gray, None)
+
+img = cv2.drawKeypoints(img_gray, kp)
+cv2.imshow('kp', img)
+cv2.waitKey(0)
