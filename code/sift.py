37082ab3a40338f8fed66ad4d5e0843a3ffb7f92
Andy <andy@example.com>
2016-11-26 20:53:49 -0800

    Updated SIFT

--- code/sift.py
+++ code/sift.py
@@ -1,14 +1,109 @@
 import cv2
 import numpy as np
+import os
 
-img = cv2.imread('Hand.jpg')
+def drawMatches(img1, kp1, img2, kp2, matches):
+    """
+    @http://stackoverflow.com/questions/20259025/module-object-has-no-attribute-drawmatches-opencv-python/26227854#26227854
+
+    My own implementation of cv2.drawMatches as OpenCV 2.4.9
+    does not have this function available but it's supported in
+    OpenCV 3.0.0
+
+    This function takes in two images with their associated 
+    keypoints, as well as a list of DMatch data structure (matches) 
+    that contains which keypoints matched in which images.
+
+    An image will be produced where a montage is shown with
+    the first image followed by the second image beside it.
+
+    Keypoints are delineated with circles, while lines are connected
+    between matching keypoints.
+
+    img1,img2 - Grayscale images
+    kp1,kp2 - Detected list of keypoints through any of the OpenCV keypoint 
+              detection algorithms
+    matches - A list of matches of corresponding keypoints through any
+              OpenCV keypoint matching algorithm
+    """
+
+    # Create a new output image that concatenates the two images together
+    # (a.k.a) a montage
+    rows1 = img1.shape[0]
+    cols1 = img1.shape[1]
+    rows2 = img2.shape[0]
+    cols2 = img2.shape[1]
+
+    out = np.zeros((max([rows1,rows2]),cols1+cols2,3), dtype='uint8')
+
+    # Place the first image to the left
+    out[:rows1,:cols1] = np.dstack([img1, img1, img1])
+
+    # Place the next image to the right of it
+    out[:rows2,cols1:] = np.dstack([img2, img2, img2])
+
+    # For each pair of points we have between both images
+    # draw circles, then connect a line between them
+    for mat in matches:
+
+        # Get the matching keypoints for each of the images
+        img1_idx = mat.queryIdx
+        img2_idx = mat.trainIdx
+
+        # x - columns
+        # y - rows
+        (x1,y1) = kp1[img1_idx].pt
+        (x2,y2) = kp2[img2_idx].pt
+
+        # Draw a small circle at both co-ordinates
+        # radius 4
+        # colour blue
+        # thickness = 1
+        cv2.circle(out, (int(x1),int(y1)), 4, (255, 0, 0), 1)   
+        cv2.circle(out, (int(x2)+cols1,int(y2)), 4, (255, 0, 0), 1)
+
+        # Draw a line in between the two points
+        # thickness = 1
+        # colour blue
+        cv2.line(out, (int(x1),int(y1)), (int(x2)+cols1,int(y2)), (255, 0, 0), 1)
+
+
+    # Show the image
+    cv2.imshow('Matched Features', out)
+    cv2.waitKey(0)
+    cv2.destroyWindow('Matched Features')
+
+    # Also return the image if you'd like a copy
+    return out
+
+img = cv2.imread('Models/Rock/1.jpg')
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 sift = cv2.SIFT()
-kp = sift.detect(img_gray, None)
+kp, des = sift.detectAndCompute(img_gray, None)
 
 img = cv2.drawKeypoints(img_gray, kp)
 cv2.imshow('kp', img)
 cv2.waitKey(0)
 
-# kp, des = sift.detectAndCompute(img_gray, None)
+test = cv2.imread('NewPhotos/train/Rock/99.jpg')
+test_gray = cv2.cvtColor(test, cv2.COLOR_BGR2GRAY)
+kp_t, des_t = sift.detectAndCompute(test_gray, None)
+cv2.imshow('kp', cv2.drawKeypoints(test_gray, kp_t))
+
+# Alg 0 = FLANN_INDEX_KDTREE
+index_params = dict(algorithm = 0, trees = 5)
+search_params = dict(checks = 50)
+
+# k nearest neighbour matcher
+flann = cv2.FlannBasedMatcher(index_params, search_params)
+matches = flann.knnMatch(des, des_t, k=2)
+
+good = []
+for (m,n) in matches:
+	#if m.distance < 0.7*n.distance:
+		good.append(m)
+
+#draw_params = dict(matchColor = (0,255,0), singlePointColor = (255,0,0), matchesMask = matchesMask, flags = 0)
+
+drawMatches(img_gray,kp,test_gray,kp_t,good)
