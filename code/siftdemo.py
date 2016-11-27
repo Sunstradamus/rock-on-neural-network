1c0470f3fede906208b79e0cade550dd4d51eecd
Andy <andy@example.com>
2016-11-26 22:17:59 -0800

    Started to refactor code

--- code/siftdemo.py
+++ code/siftdemo.py
@@ -32,24 +32,93 @@ for classname in TRAIN_FILES:
 	print 'Created keypoints for class "' + classname + '"'
 print
 
-print 'Loading random class test images'
-for image in sorted(os.listdir(TEST_DIR+'/Rock')):
-	img = cv2.imread(TEST_DIR+'/Rock/'+image)
-	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	#print 'Creating keypoints'
-	kp, des = sift.detectAndCompute(img, None)
-	#print 'Calculating best match'
-	max_count = 0
-	classifier = ''
-	for classname in train_data:
-		for (kp_t,des_t) in train_data[classname]:
-			matches = matcher.knnMatch(des, des_t, k=2)
+# print 'Loading Rock class test images'
+# cntr = 0
+# fail = 0
+# for image in sorted(os.listdir(TEST_DIR+'/Rock')):
+# 	img = cv2.imread(TEST_DIR+'/Rock/'+image)
+# 	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# 	#print 'Creating keypoints'
+# 	kp, des = sift.detectAndCompute(img, None)
+# 	#print 'Calculating best match'
+# 	max_count = 0
+# 	classifier = ''
+# 	for classname in train_data:
+# 		for (kp_t,des_t) in train_data[classname]:
+# 			matches = matcher.knnMatch(des, des_t, k=2)
 
-			count = 0
-			for (m,n) in matches:
-				if m.distance < 0.8*n.distance:
-					count += 1
-			if count > max_count:
-				max_count = count
-				classifier = classname
-	print 'Best match ('+image+'): ' + classifier + ' with score ' + str(max_count)
+# 			count = 0
+# 			for (m,n) in matches:
+# 				if m.distance < 0.8*n.distance:
+# 					count += 1
+# 			if count > max_count:
+# 				max_count = count
+# 				classifier = classname
+# 	if classifier != 'Rock':
+# 		fail += 1
+# 	cntr += 1
+# 	print 'Best match ('+image+'): ' + classifier + ' with score ' + str(max_count)
+# print 'Failure rate ' + str(fail) + '/' + str(cntr)
+
+print 'Loading Paper class test images'
+min_fail = 99
+min_delta = 0
+for delta in np.arange(0.5,1,0.01):
+	cntr = 0
+	fail = 0
+	for image in sorted(os.listdir(TEST_DIR+'/Paper')):
+		img = cv2.imread(TEST_DIR+'/Paper/'+image)
+		img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		#print 'Creating keypoints'
+		kp, des = sift.detectAndCompute(img, None)
+		#print 'Calculating best match'
+		max_count = 0
+		classifier = ''
+		for classname in train_data:
+			for (kp_t,des_t) in train_data[classname]:
+				matches = matcher.knnMatch(des, des_t, k=2)
+
+				count = 0
+				for (m,n) in matches:
+					if m.distance < delta*n.distance:
+						count += 1
+				if count > max_count:
+					max_count = count
+					classifier = classname
+		if classifier != 'Paper':
+			fail += 1
+		cntr += 1
+		#print 'Best match ('+image+'): ' + classifier + ' with score ' + str(max_count)
+		if fail < min_fail:
+			min_fail = fail
+			min_delta = delta
+	print 'Failure rate ('+str(delta)+') ' + str(fail) + '/' + str(cntr)
+print 'Min delta ' + str(min_delta) + ' with failures ' + str(min_fail)
+
+# print 'Loading Scissors class test images'
+# cntr = 0
+# fail = 0
+# for image in sorted(os.listdir(TEST_DIR+'/Scissors')):
+# 	img = cv2.imread(TEST_DIR+'/Scissors/'+image)
+# 	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# 	#print 'Creating keypoints'
+# 	kp, des = sift.detectAndCompute(img, None)
+# 	#print 'Calculating best match'
+# 	max_count = 0
+# 	classifier = ''
+# 	for classname in train_data:
+# 		for (kp_t,des_t) in train_data[classname]:
+# 			matches = matcher.knnMatch(des, des_t, k=2)
+
+# 			count = 0
+# 			for (m,n) in matches:
+# 				if m.distance < 0.8*n.distance:
+# 					count += 1
+# 			if count > max_count:
+# 				max_count = count
+# 				classifier = classname
+# 	if classifier != 'Scissors':
+# 		fail += 1
+# 	cntr += 1
+# 	print 'Best match ('+image+'): ' + classifier + ' with score ' + str(max_count)
+# print 'Failure rate ' + str(fail) + '/' + str(cntr)
