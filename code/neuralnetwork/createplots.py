c63724b1b107e01cfaeb29befc990b320e124ffd
Jacob Patenaude <jacob.patenaude@teampages.com>
2016-12-03 22:23:30 -0800

    Change plot style to be all in one

--- code/neuralnetwork/createplots.py
+++ code/neuralnetwork/createplots.py
@@ -1,23 +1,32 @@
 import os
 import matplotlib.pyplot as plt
 
-for directory in os.listdir('.'):
-  if os.path.isfile(directory): continue
+train_acc = dict()
+test_acc = dict()
 
+directories = [x for x in sorted(os.listdir('.')) if (not os.path.isfile(x) and x != '.DS_Store')]
+
+for directory in directories:
   infile = open(directory + "/output.txt", 'r')
 
-  train_acc = []
-  test_acc = []
+  train_acc[directory] = []
+  test_acc[directory] = []
 
   for line in infile.readlines():
       if line.find("acc") == -1: continue
-      train_acc.append(float(line.split()[6]))
-      test_acc.append(float(line.split()[12]))
+      train_acc[directory].append(float(line.split()[6]))
+      test_acc[directory].append(float(line.split()[12]))
 
   infile.close()
 
-  plt.clf()
-  plt.plot(train_acc)
-  plt.plot(test_acc)
-  plt.legend(["training accuracy", "testing accuracy"], loc="best")
-  plt.savefig(directory + '/' + directory + '_plot.png')
+plt.clf()
+for directory in directories:
+  plt.plot(train_acc[directory], label=directory)
+  plt.legend(loc="best")
+  plt.savefig('train_acc.png')
+
+plt.clf()
+for directory in directories:
+  plt.plot(test_acc[directory], label=directory)
+  plt.legend(loc="best")
+  plt.savefig('test_acc.png')
